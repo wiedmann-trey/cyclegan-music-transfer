1a95ccbc708cbd0652a98a22a6bea4a10580dc86
carolinebzhang <caroline_b_zhang@brown.edu>
2023-04-21 08:15:17 -0400

    new branch for me

--- models.py
+++ models.py
@@ -7,22 +7,108 @@ from mido import MidiFile
 import numpy as np
 import torch 
 import torch.nn as nn
+import math
 
 class Discriminator(nn.Module):
 
-    def __init__(self, input_nc, batch_size=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_bias=False):
-            self.batch_size = batch_size
+    def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p):
+            
+            super().__init__()
 
-    def forward(self, input):
-          return
-    
+            self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p)
+
+            self.model_type = "Transformer"
+            self.dim_model = dim_model
+
+        # LAYERS
+            self.positional_encoder = PositionalEncoding(
+            dim_model=dim_model, dropout_p=dropout_p, max_len=5000
+        )
+            self.embedding = nn.Embedding(num_tokens, dim_model)
+            self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p,
+        )
+            self.out = nn.Linear(dim_model, num_tokens)
+
+    def forward(self,src,tgt):
+        # Src size must be (batch_size, src sequence length)
+        # Tgt size must be (batch_size, tgt sequence length)
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+
+        # we permute to obtain size (sequence length, batch_size, dim_model),
+        src = src.permute(1, 0, 2)
+        tgt = tgt.permute(1, 0, 2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt)
+        out = self.out(transformer_out)
+
+        return out
+
 
 class Generator(nn.Module):
-    def __init__(self, input_nc, batch_size=64, n_layers=3, norm_layer=nn.BatchNorm2d, use_bias=False):
-            self.batch_size = batch_size
+    def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p):
+            
+            super().__init__()
 
-    def forward(self, input):
-        return input
+            self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p)
+
+            self.model_type = "Transformer"
+            self.dim_model = dim_model
+
+        # LAYERS
+            self.positional_encoder = PositionalEncoding(
+            dim_model=dim_model, dropout_p=dropout_p, max_len=5000
+        )
+            self.embedding = nn.Embedding(num_tokens, dim_model)
+            self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p,
+        )
+            self.out = nn.Linear(dim_model, num_tokens)
+
+    def forward(self,src,tgt):
+        # Src size must be (batch_size, src sequence length)
+        # Tgt size must be (batch_size, tgt sequence length)
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+
+        # we permute to obtain size (sequence length, batch_size, dim_model),
+        src = src.permute(1, 0, 2)
+        tgt = tgt.permute(1, 0, 2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt)
+        out = self.out(transformer_out)
+
+        return out
+
 
 class Classifier(nn.Module):
     def __init__(self, dim=64):
@@ -70,3 +156,68 @@ class CycleGAN(nn.Module):
                     DB_fake_all = self.D_B_all(sample_fake_B + gauss_noise)   
 
                     #https://github.com/Asthestarsfalll/Symbolic-Music-Genre-Transfer-with-CycleGAN-for-pytorch/blob/main/model.py      
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        # max_len determines how far the position can have an effect on a token (window)
+        
+        # Info
+        self.dropout = nn.Dropout(dropout_p)
+        
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+        
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+        
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding",pos_encoding)
+        
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+    
+
+def accuracy_function(prbs, labels, mask):
+    """
+    DO NOT CHANGE
+
+    Computes the batch accuracy
+
+    :param prbs:  float tensor, word prediction probabilities [BATCH_SIZE x WINDOW_SIZE x VOCAB_SIZE]
+    :param labels:  integer tensor, word prediction labels [BATCH_SIZE x WINDOW_SIZE]
+    :param mask:  tensor that acts as a padding mask [BATCH_SIZE x WINDOW_SIZE]
+    :return: scalar tensor of accuracy of the batch between 0 and 1
+    """
+    correct_classes = tf.argmax(prbs, axis=-1) == labels
+    accuracy = tf.reduce_mean(tf.boolean_mask(tf.cast(correct_classes, tf.float32), mask))
+    return accuracy
+
+
+def loss_function(prbs, labels, mask):
+    """
+    DO NOT CHANGE
+
+    Calculates the model cross-entropy loss after one forward pass
+    Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
+
+    :param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+    :param labels:  integer tensor, word prediction labels [batch_size x window_size]
+    :param mask:  tensor that acts as a padding mask [batch_size x window_size]
+    :return: the loss of the model as a tensor
+    """
+    masked_labs = tf.boolean_mask(labels, mask)
+    masked_prbs = tf.boolean_mask(prbs, mask)
+    scce = tf.keras.losses.sparse_categorical_crossentropy(masked_labs, masked_prbs, from_logits=True)
+    loss = tf.reduce_sum(scce)
+    return loss
+    
--- preprocess.py
+++ preprocess.py
@@ -3,10 +3,11 @@ import os
 import pretty_midi
 import shutil
 import muspy 
-from mido import MidiFile
+from mido import MidiFile, MidiTrack, Message
 import numpy as np
 import torch as torch
-
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
 
 def create_song_genre_dict(root):
     """
@@ -45,12 +46,6 @@ def get_genre_path(root):
 
 lakh_song_folders, song_genre_dict = get_genre_path("lmd_matched")
 
-def add_augmentation(muspy_object):
-    # tbh I'm not super sure what adjust resolution this is kinda a filler add augmentation function, but we might
-    # have to augment midi instead of muspy object
-    if np.random.random() < 0.1:
-        return muspy_object.adjust_resolution(target=1, factor=0.1)
-    return muspy_object
 
 def create_genre_number_dict(root):
     """creates a dictionary mapping all the string labels to a number which we can later use for one hot encoding"""
@@ -64,60 +59,160 @@ def create_genre_number_dict(root):
 
 genre_number_dict = create_genre_number_dict("labels for songs")
 
-def create_muspy(lakh_song_folders, maestro_path):
-    '''goes through all of the lakh song folder contents and creates muspy objects.
-    also goes through the main maestro folder and creates muspy objects
-    returns all of the muspy objects from both lakh and yamaha, and moves corrupt midi files to 
-    a separate folder'''
-    muspy_objects = []
-    invalid_mids = []
-    corresponding_labels = []
-    os.mkdir("invalid_midis")
-    for song_path in lakh_song_folders:
-        for dirpath, dirs, midifiles in os.walk(song_path):
-            for midifile in midifiles: 
-                absolute_song_path = os.path.join(song_path, midifile)
-                try: 
-                    just_song_key = song_genre_dict[song_path]
-                    song_genre = genre_dictionary[just_song_key]
-                    if song_genre != "unknown":
-                        muspy_obj = muspy.read(absolute_song_path)
-                        muspy_obj = add_augmentation(muspy_obj)
-                        muspy_obj.annotations = song_genre
-                        muspy_obj = muspy.to_event_representation(music=muspy_obj, encode_velocity=True, max_time_shift=100, velocity_bins=32)
-                        muspy_objects.append(muspy_obj)
-                        numerical_label = genre_number_dict[song_genre]
-                        corresponding_labels.append(numerical_label)
+def augment_pitch(muspy_object):
+    if np.random.random() < 0.1:
+        muspy_object = muspy_object.transpose(np.random.randint(-2, 2))
+    return muspy_object
+
+def split_midi(midi_path, time_interval, header):
+    """given a midi file path and a time interval, divides the midi file into sub-midi-files of that time interval.
+    Returns a list of muspy music objects of that time interval"""
+    original_mido = MidiFile(midi_path, clip=True) # a lot of the LAKH ones were erroring since some of the velocities were over 127 so im clipping them w mido
+    split_midi_files = [] 
+    split_muspy_events = []
+    split_song_labels = []
+    split_testing_muspy = []
+    total_seconds = original_mido.length #length of midos are given in seconds 
+    ticks_per_beat = original_mido.ticks_per_beat 
+    total_ticks = 0
+    try: 
+        just_song_key = song_genre_dict[header]
+        song_genre = genre_dictionary[just_song_key]
+        song_genre = genre_number_dict[song_genre]
+    except Exception as e:
+        song_genre = genre_number_dict["classical"]
+
+    #calculate total number of ticks
+    for msg in original_mido.tracks[1]: #have to start at index one since the first track is a header so it doesn't have any time ticks (apparently)
+        if msg.time:
+            total_ticks += msg.time
+
+    total_beats = total_ticks / ticks_per_beat
+    ticks_per_second = (total_beats / total_seconds) * ticks_per_beat
+
+    scale = np.random.random()
+    scaling_factor = 1
+
+    if scale < 0.1:
+        scaling_factor = 1.2
+    elif scale > 0.9:
+        scaling_factor = 0.8
+
+    for i in range(int((original_mido.length * scaling_factor) // time_interval)):
+        new_split_mido = MidiFile()
+        lower_range = i * time_interval
+        upper_range = (i + 1) * time_interval 
+
+        for track in original_mido.tracks:
+            split_track = MidiTrack()
+            current_msg_time=0
+
+            for msg in track:
+                current_msg_time += (int(np.round(msg.time * scaling_factor))/ ticks_per_second)
+
+                if lower_range <= current_msg_time < upper_range: 
+                    #msg.time = msg.time * scaling_factor
+                    if hasattr(msg, "instrument_name"):
+                        "has instrument name"
+                        if msg.instrument_name != "drums" and msg.instrument_name != "drum" and msg.instrument_name != "percussion":
+                            split_track.append(msg)
                     else:
-                        continue
+                        split_track.append(msg)
+
+            new_split_mido.tracks.append(split_track)
+        split_midi_files.append(new_split_mido)
+    
+    string1 = "/Users/carolinezhang/Downloads/cyclegan-music-transfer/lol test folder/subtle" 
+    end = ".mid"
+    new_file = "1"
+    for split_midi in split_midi_files:
+        #finalpath = string1 + new_file + end
+        split_mus = muspy.from_mido(split_midi)
+        new_mus = muspy.Music()
+        for i in split_mus.tracks:
+            if not i.is_drum:
+                new_mus.tracks.append(i)
+        #muspy.outputs.save(finalpath, split_mus)
+        #muspy.outputs.write(path=finalpath, music=split_mus)
+        #split_mus = augment_pitch(split_mus)
+        split_mus = muspy.to_event_representation(new_mus, use_end_of_sequence_event=False)
+        split_muspy_events.append(split_mus)
+        split_song_labels.append(song_genre)
+        new_file += "1"
+        
+    return split_muspy_events, split_song_labels
+
+def get_event_representations(lakh_paths:list, yamaha_path:str, time_interval:int):
+    '''converts midis in lakh dataset to muspy event representation'''
+    timeshifts = []
+    labels = []
+    invalid_data = []
+    for lakh_path in lakh_paths:
+        for dirpath, dirs, midifiles in os.walk(lakh_path):
+            for midifile in midifiles: 
+                absolute_song_path = os.path.join(lakh_path, midifile)
+                try: 
+                    lakh_timeshifts, lakh_labels = split_midi(absolute_song_path, time_interval, lakh_path)
+                    timeshifts.extend(lakh_timeshifts)
+                    labels.extend(lakh_labels)
+                    print("lakh not corrupted!")
+                    print(labels)
                 except Exception as e:
                     print(repr(absolute_song_path))
-                    invalid_mids.append(absolute_song_path)
-                    shutil.move(absolute_song_path, "invalid_midis")
+                    invalid_data.append(absolute_song_path)
                     continue
-    #maybe theres an easier way to do this but like we have to use folder names to ID midi files in LAKH whereas in
-    #yamaha everything in classical so I just used os.walk on two paths instead of putting them all in one big folder. 
-    for dirpath, dirs, filenames in os.walk(maestro_path):
-        for filename in filenames: 
-            if filename.endswith(".midi") or filename.endswith(".mid"):
-                rel_song_path = os.path.join(dirpath, filename)
+    for dirpath, dirs, midifiles in os.walk(yamaha_path):
+            for midifile in midifiles: 
+                absolute_song_path = os.path.join(yamaha_path, midifile)
                 try: 
-                    new_yamaha = muspy.read(rel_song_path)
-                    new_yamaha = add_augmentation(new_yamaha)
-                    song_genre = "classical"
-                    new_yamaha.annotations = song_genre
-                    new_yamaha = muspy.to_event_representation(music=new_yamaha, encode_velocity=True, max_time_shift=100, velocity_bins=32)
-                    muspy_objects.append(new_yamaha) 
-                    numerical_label = genre_number_dict[song_genre]
-                    corresponding_labels.append(numerical_label)
+                    yamaha_timeshifts, yamaha_labels = split_midi(absolute_song_path, time_interval, midifile)
+                    timeshifts.extend(yamaha_timeshifts)
+                    labels.extend(yamaha_labels)
+                    print("yamaha not corrupted!")
+                    print(labels)
                 except Exception as e:
-                    print(repr(filename))
-                    shutil.move(rel_song_path, "invalid_midis")
+                    print(repr(absolute_song_path))
+                    invalid_data.append(absolute_song_path)
                     continue
-    return muspy_objects, corresponding_labels
+    #just cuz I wanted to see what it looked like
+    with open("muspy_labels.txt", "w") as output:
+        output.write(str(labels))
+    return timeshifts, labels
+                
+total_timeshifts, total_labels = get_event_representations(song_paths, "maestro-v3.0.0", 30)
 
 
-#create_muspy("lmd_matched", "maestro-v3.0.0")
-            
+def get_test_train_samples(all_timeshifts, all_labels, first_class, second_class, num_classes):
+    first_genre_ts = []
+    second_genre_ts = []
+    for i in range(len(all_labels)):
+        if all_labels[i] == first_class or all_labels[i] == second_class:
+                first_genre_ts.append(all_timeshifts[i])
+        if all_labels[i] == second_class:
+              second_genre_ts.append(all_timeshifts[i])
+    first_genre_ts = np.asarray(first_genre_ts)
+    second_genre_ts = np.asarray(second_genre_ts)
+    genre_labels = np.array(genre_labels)
+    genre_labels = torch.from_numpy(genre_labels)
+    genre_labels = torch.nn.functional.one_hot(genre_labels, num_classes)
+    return first_genre_ts, second_genre_ts
 
+class TimeShiftDataset(Dataset):
+    def __init__(self, time_shifts):
+        self.time_shifts = time_shifts
 
+    def __len__(self):
+        return len(self.time_shifts)
+
+def get_data():
+    song_paths, song_genre_dict = get_genre_path("lmd_matched")
+    genre_number_dict = create_genre_number_dict("labels for songs")
+    total_timeshifts, total_labels = get_event_representations(song_paths, "maestro-v3.0.0", 30)
+
+    pop_rock_samples, classical_samples = get_test_train_samples(total_timeshifts, total_labels, 2, 13, 15)
+    pop_rock_data = TimeShiftDataset(time_shifts=pop_rock_samples)
+    classical_data = TimeShiftDataset(time_shifts=classical_samples)
+
+    classical = DataLoader(dataset=classical_data, batch_size=32, shuffle=True)
+    pop_rock = DataLoader(dataset=classical_data, batch_size=32, shuffle=True)
+    return classical, pop_rock
