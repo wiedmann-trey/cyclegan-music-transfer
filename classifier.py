7553f9a771c082a731dcd0eb72f4dca624c06433
carolinebzhang <caroline_b_zhang@brown.edu>
2023-05-02 10:19:31 -0400

    printing every 100 batches not every 5 lol

--- classifier.py
+++ classifier.py
@@ -103,8 +103,8 @@ def train(model):
             running_loss += loss.item()
     
             # print statistics
-            if i % 5 == 1:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:10d}] loss: {running_loss / 5:.10f}')
+            if i % 100 == 1:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:10d}] loss: {running_loss / 100:.10f}')
                 running_loss = 0.0
         
         # save every epoch
