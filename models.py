1185c8816096fb33d43ea9d44783d689befc6a5d
wiedmann-trey <trewiedmann@gmail.com>
2023-05-01 21:17:17 -0400

    LMFAO

--- models.py
+++ models.py
@@ -84,7 +84,7 @@ class Generator(nn.Module):
         outputs = torch.zeros(batch_size, max_len, vocab_size).cuda() #, requires_grad=False)
         max_output = torch.zeros(batch_size, max_len).cuda() #, requires_grad=False)
 
-        decoder_input = torch.zeros(batch_size, dtype=torch.long).cuda() #, requires_grad=False)
+        decoder_input = torch.zeros(batch_size, dtype=torch.int32).cuda() #, requires_grad=False)
         max_output[:,0] = decoder_input
 
         for t in range(max_len):
